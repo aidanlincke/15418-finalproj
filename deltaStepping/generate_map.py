3c94efa7851ffd105b80b97408e10d7bea3735fe
Aidan Lincke <aidan@lincke.org>
2025-04-29 01:39:01 -0400

    Rename + running code instructions

---
+++ deltaStepping/generate_map.py
@@ -0,0 +1,58 @@
+from pyproj import Transformer
+from rasterio.windows import from_bounds
+import rasterio
+import numpy as np
+import struct
+from skimage.transform import resize
+from affine import Affine
+
+def world_to_pixel(transform, x, y):
+    col = int((x - transform.c) / transform.a)
+    row = int((y - transform.f) / transform.e)
+    return row, col
+
+if __name__ == "__main__":
+    with rasterio.open("problems/dc.tif") as tif:
+        transformer = Transformer.from_crs("EPSG:4326", tif.crs, always_xy=True)
+
+        wm_x, wm_y = transformer.transform(-77.0353, 38.8895) # Washington Monument
+        dca_x, dca_y = transformer.transform(-77.0402, 38.8512) # DCA
+
+        x_min = min(wm_x, dca_x) - 100
+        x_max = max(wm_x, dca_x) + 100
+        y_min = min(wm_y, dca_y) - 100
+        y_max = max(wm_y, dca_y) + 100
+
+        window = from_bounds(x_min, y_min, x_max, y_max, tif.transform)
+        cropped = tif.read(1, window=window)
+
+    cropped = np.nan_to_num(cropped, nan=0.0).astype(np.float32)
+    cropped -= np.min(cropped) # Normalize to >=0
+
+    original_rows, original_cols = cropped.shape
+    target_rows = 2500
+    target_cols = int(original_cols * (target_rows / original_rows))
+
+    resized = resize(
+        cropped, 
+        (target_rows, target_cols), 
+        order=1, 
+        preserve_range=True, 
+        anti_aliasing=True
+    ).astype(np.float32)
+
+    cropped = resized
+    rows, cols = cropped.shape
+
+    scale_x = (x_max - x_min) / cols
+    scale_y = (y_max - y_min) / rows
+    transform = Affine.translation(x_min, y_min) * Affine.scale(scale_x, scale_y)
+
+    start_row, start_col = world_to_pixel(transform, dca_x, dca_y)
+    goal_row, goal_col = world_to_pixel(transform, wm_x, wm_y)
+
+    with open("problems/dc.bin", "wb") as f:
+        f.write(struct.pack("ii", rows, cols))
+        f.write(struct.pack("ii", start_row, start_col))
+        f.write(struct.pack("ii", goal_row, goal_col))
+        f.write(cropped.tobytes())
---
+++ deltaStepping/visualizer.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import struct
+
+
+def read_problem(filepath):
+    with open(filepath, "rb") as f:
+        header = f.read(6 * 4)
+        rows, cols, start_row, start_col, end_row, end_col = struct.unpack("iiiiii", header)
+
+        data = f.read(rows * cols * 4)
+        map_flat = np.frombuffer(data, dtype=np.float32)
+        map = map_flat.reshape((rows, cols))
+
+    return map, (start_row, start_col), (end_row, end_col)
+
+
+def read_plan(filepath):
+    with open(filepath, "rb") as f:
+        length_bytes = f.read(4)
+        length = struct.unpack("i", length_bytes)[0]
+
+        plan = [
+            struct.unpack("ii", f.read(8))
+            for _ in range(length)
+        ]
+
+    return plan
+
+if __name__ == "__main__":
+    map, (start_row, start_col), (end_row, end_col) = read_problem("problems/dc.bin")
+
+    plan = read_plan('plans/dc.bin')
+    plan_rows = [r for r, _ in plan]
+    plan_cols = [c for _, c in plan]
+
+    plt.figure(figsize=(10, 10))
+    plt.imshow(map, cmap='gray', origin='upper')
+
+    plt.plot(plan_cols, plan_rows, color='blue', linewidth=1, label="Path")
+
+    plt.scatter([start_col], [start_row], color='green', s=50, label="Start")
+    plt.scatter([end_col], [end_row], color='red', s=50, label="Goal")
+
+    plt.legend()
+    plt.axis("off")
+    plt.tight_layout()
+    plt.show()
